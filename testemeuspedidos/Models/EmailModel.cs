22bafc9484f74260e7a88a96e4e869119fff747d
leandro <lesvasconcelos@gmail.com>
2017-04-30 01:47:13 -0300

    first commit

---
+++ testemeuspedidos/Models/EmailModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+
+namespace testemeuspedidos.Models
+{
+    public enum EmailPerfil
+    {
+        FrontEnd,
+        BackEnd,
+        Mobile,
+        Generico
+    }
+
+    public class EmailModel
+    {
+        public const string EMAIL_REMETENTE = "testedevmp@gmail.com";
+        public static void Enviar(EmailPerfil emailPerfil, string destinatario)
+        {
+            var mail = MailMessageFromEmailPerfil(emailPerfil);
+            mail.To.Add(new MailAddress(destinatario));
+
+            var smtp = new SmtpClient("smtp.gmail.com");
+            smtp.EnableSsl = true;
+            smtp.Port = 587;
+            smtp.Credentials = new NetworkCredential(EMAIL_REMETENTE, "testeDevMP123");
+            smtp.Send(mail);
+        }
+
+        public static MailMessage MailMessageFromEmailPerfil(EmailPerfil emailPerfil)
+        {
+            var mail = new MailMessage();
+            var corpoGenerico = "Obrigado por se candidatar, assim que tivermos uma vaga disponível para programador {0}entraremos em contato.";
+            mail.From = new MailAddress(EMAIL_REMETENTE, "Teste MP");
+            mail.Subject = "Obrigado por se candidatar";
+
+            switch (emailPerfil)
+            {
+                case EmailPerfil.BackEnd:
+                    mail.Body = string.Format(corpoGenerico, "Back-End ");
+                    break;
+                case EmailPerfil.FrontEnd:
+                    mail.Body = string.Format(corpoGenerico, "Front-End ");
+                    break;
+                case EmailPerfil.Generico:
+                    mail.Body = string.Format(corpoGenerico, string.Empty);
+                    break;
+                case EmailPerfil.Mobile:
+                    mail.Body = string.Format(corpoGenerico, "Mobile ");
+                    break;
+            }
+
+            return mail;
+        }
+        
+    }
+}
---
+++ testemeuspedidos/Models/HomeViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testemeuspedidos.Models
+{
+    public class HomeViewModel
+    {
+        [Required(ErrorMessage = "O nome é obrigatório", AllowEmptyStrings = false)]
+        [Display(Name ="Nome completo")]
+        public string Nome { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name ="E-mail")]
+        public string Email { get; set; }
+
+        [Range(0, 10,ErrorMessage = "O valor precisa estar no intervalo de 0 a 10")]
+        public int? HTML { get; set; }
+
+        [Range(0, 10, ErrorMessage = "O valor precisa estar no intervalo de 0 a 10")]
+        public int? CSS { get; set; }
+
+        [Range(0, 10, ErrorMessage = "O valor precisa estar no intervalo de 0 a 10")]
+        public int? Javascript { get; set; }
+
+        [Range(0, 10, ErrorMessage = "O valor precisa estar no intervalo de 0 a 10")]
+        public int? Python { get; set; }
+
+        [Range(0, 10, ErrorMessage = "O valor precisa estar no intervalo de 0 a 10")]
+        public int? Django { get; set; }
+
+        [Range(0, 10, ErrorMessage = "O valor precisa estar no intervalo de 0 a 10")]
+        public int? DesenvolvimentoIOS { get; set; }
+
+        [Range(0, 10, ErrorMessage = "O valor precisa estar no intervalo de 0 a 10")]
+        public int? DesenvolvimentoAndroid { get; set; }
+
+        public void ProcessarCandidatura()
+        {
+            var nenhumPerfil = true;
+
+            if (TemPontuacaoMinima(HTML, CSS, Javascript))
+            {
+                EmailModel.Enviar(EmailPerfil.FrontEnd, Email);
+                nenhumPerfil &= false;
+            }
+            if (TemPontuacaoMinima(Python, Django))
+            {
+                EmailModel.Enviar(EmailPerfil.BackEnd, Email);
+                nenhumPerfil &= false;
+            }
+            if (TemPontuacaoMinima(DesenvolvimentoAndroid, DesenvolvimentoIOS))
+            {
+                EmailModel.Enviar(EmailPerfil.Mobile, Email);
+                nenhumPerfil &= false;
+            }
+
+            if (nenhumPerfil)
+            {
+                EmailModel.Enviar(EmailPerfil.Generico, Email);
+            }
+        }
+
+        private bool TemPontuacaoMinima(params int?[] conhecimentos)
+        {
+            return conhecimentos.All(w => w.HasValue && w.Value >= 7 && w.Value <= 10);
+        }
+    }
+}
